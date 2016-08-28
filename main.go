1cd7fb7bb0c5710d2dc96c583288c434f87158cf
shingo INADA <mecha.is06@gmail.com>
2016-08-28 16:46:57 +0000

    update

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"strconv"
+	"time"
+
 	"github.com/go-martini/martini"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/martini-contrib/render"
 	"github.com/martini-contrib/sessions"
-	"net/http"
-	"strconv"
 )
 
-var db *sql.DB
 var (
+	cpuProfileFile   = "/tmp/cpu.pprof"
+	memProfileFile   = "/tmp/mem.pprof"
+	blockProfileFile = "/tmp/block.pprof"
+
+	db *sql.DB
+
 	UserLockThreshold int
 	IPBanThreshold    int
+
+	ErrBannedIP      = errors.New("Banned IP")
+	ErrLockedUser    = errors.New("Locked user")
+	ErrUserNotFound  = errors.New("Not found user")
+	ErrWrongPassword = errors.New("Wrong password")
 )
 
 func init() {
@@ -46,12 +65,52 @@ func init() {
 }
 
 func main() {
+	runtime.MemProfileRate = 1024
+	http.HandleFunc("/startprof", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Create(cpuProfileFile)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		runtime.SetBlockProfileRate(1) // どれくらい遅くなるか確認する
+		w.Write([]byte("profile started\n"))
+	})
+
+	http.HandleFunc("/endprof", func(w http.ResponseWriter, r *http.Request) {
+		pprof.StopCPUProfile()
+		runtime.SetBlockProfileRate(0)
+		w.Write([]byte("profile ended\n"))
+
+		mf, err := os.Create(memProfileFile)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		pprof.WriteHeapProfile(mf)
+
+		bf, err := os.Create(blockProfileFile)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		pprof.Lookup("block").WriteTo(bf, 0)
+	})
+
+	go func() {
+		log.Println("Listen 6060")
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	m := martini.Classic()
 
 	store := sessions.NewCookieStore([]byte("secret-isucon"))
 	m.Use(sessions.Sessions("isucon_go_session", store))
 
-	m.Use(martini.Static("../public"))
+	m.Use(martini.Static("./public"))
 	m.Use(render.Renderer(render.Options{
 		Layout: "layout",
 	}))
@@ -104,4 +163,334 @@ func main() {
 	})
 
 	http.ListenAndServe(":8080", m)
+}
+
+func getEnv(key string, def string) string {
+	v := os.Getenv(key)
+	if len(v) == 0 {
+		return def
+	}
+
+	return v
+}
+
+func getFlash(session sessions.Session, key string) string {
+	value := session.Get(key)
+
+	if value == nil {
+		return ""
+	} else {
+		session.Delete(key)
+		return value.(string)
+	}
+}
+
+func calcPassHash(password, hash string) string {
+	h := sha256.New()
+	io.WriteString(h, password)
+	io.WriteString(h, ":")
+	io.WriteString(h, hash)
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+type User struct {
+	ID           int
+	Login        string
+	PasswordHash string
+	Salt         string
+
+	LastLogin *LastLogin
+}
+
+type LastLogin struct {
+	Login     string
+	IP        string
+	CreatedAt time.Time
+}
+
+func (u *User) getLastLogin() *LastLogin {
+	rows, err := db.Query(
+		"SELECT login, ip, created_at FROM login_log WHERE succeeded = 1 AND user_id = ? ORDER BY id DESC LIMIT 2",
+		u.ID,
+	)
+
+	if err != nil {
+		return nil
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		u.LastLogin = &LastLogin{}
+		err = rows.Scan(&u.LastLogin.Login, &u.LastLogin.IP, &u.LastLogin.CreatedAt)
+		if err != nil {
+			u.LastLogin = nil
+			return nil
+		}
+	}
+
+	return u.LastLogin
+}
+
+func createLoginLog(succeeded bool, remoteAddr, login string, user *User) error {
+	succ := 0
+	if succeeded {
+		succ = 1
+	}
+
+	var userId sql.NullInt64
+	if user != nil {
+		userId.Int64 = int64(user.ID)
+		userId.Valid = true
+	}
+
+	_, err := db.Exec(
+		"INSERT INTO login_log (`created_at`, `user_id`, `login`, `ip`, `succeeded`) "+
+			"VALUES (?,?,?,?,?)",
+		time.Now(), userId, login, remoteAddr, succ,
+	)
+
+	return err
+}
+
+func isLockedUser(user *User) (bool, error) {
+	if user == nil {
+		return false, nil
+	}
+
+	var ni sql.NullInt64
+	row := db.QueryRow(
+		"SELECT COUNT(1) AS failures FROM login_log WHERE "+
+			"user_id = ? AND id > IFNULL((select id from login_log where user_id = ? AND "+
+			"succeeded = 1 ORDER BY id DESC LIMIT 1), 0);",
+		user.ID, user.ID,
+	)
+	err := row.Scan(&ni)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, err
+	}
+
+	return UserLockThreshold <= int(ni.Int64), nil
+}
+
+func isBannedIP(ip string) (bool, error) {
+	var ni sql.NullInt64
+	row := db.QueryRow(
+		"SELECT COUNT(1) AS failures FROM login_log WHERE "+
+			"ip = ? AND id > IFNULL((select id from login_log where ip = ? AND "+
+			"succeeded = 1 ORDER BY id DESC LIMIT 1), 0);",
+		ip, ip,
+	)
+	err := row.Scan(&ni)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, err
+	}
+
+	return IPBanThreshold <= int(ni.Int64), nil
+}
+
+func attemptLogin(req *http.Request) (*User, error) {
+	succeeded := false
+	user := &User{}
+
+	loginName := req.PostFormValue("login")
+	password := req.PostFormValue("password")
+
+	remoteAddr := req.RemoteAddr
+	if xForwardedFor := req.Header.Get("X-Forwarded-For"); len(xForwardedFor) > 0 {
+		remoteAddr = xForwardedFor
+	}
+
+	defer func() {
+		createLoginLog(succeeded, remoteAddr, loginName, user)
+	}()
+
+	row := db.QueryRow(
+		"SELECT id, login, password_hash, salt FROM users WHERE login = ?",
+		loginName,
+	)
+	err := row.Scan(&user.ID, &user.Login, &user.PasswordHash, &user.Salt)
+
+	switch {
+	case err == sql.ErrNoRows:
+		user = nil
+	case err != nil:
+		return nil, err
+	}
+
+	if banned, _ := isBannedIP(remoteAddr); banned {
+		return nil, ErrBannedIP
+	}
+
+	if locked, _ := isLockedUser(user); locked {
+		return nil, ErrLockedUser
+	}
+
+	if user == nil {
+		return nil, ErrUserNotFound
+	}
+
+	if user.PasswordHash != calcPassHash(password, user.Salt) {
+		return nil, ErrWrongPassword
+	}
+
+	succeeded = true
+	return user, nil
+}
+
+func getCurrentUser(userId interface{}) *User {
+	user := &User{}
+	row := db.QueryRow(
+		"SELECT id, login, password_hash, salt FROM users WHERE id = ?",
+		userId,
+	)
+	err := row.Scan(&user.ID, &user.Login, &user.PasswordHash, &user.Salt)
+
+	if err != nil {
+		return nil
+	}
+
+	return user
+}
+
+func bannedIPs() []string {
+	ips := []string{}
+
+	rows, err := db.Query(
+		"SELECT ip FROM "+
+			"(SELECT ip, MAX(succeeded) as max_succeeded, COUNT(1) as cnt FROM login_log GROUP BY ip) "+
+			"AS t0 WHERE t0.max_succeeded = 0 AND t0.cnt >= ?",
+		IPBanThreshold,
+	)
+
+	if err != nil {
+		return ips
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var ip string
+
+		if err := rows.Scan(&ip); err != nil {
+			return ips
+		}
+		ips = append(ips, ip)
+	}
+	if err := rows.Err(); err != nil {
+		return ips
+	}
+
+	rowsB, err := db.Query(
+		"SELECT ip, MAX(id) AS last_login_id FROM login_log WHERE succeeded = 1 GROUP by ip",
+	)
+
+	if err != nil {
+		return ips
+	}
+
+	defer rowsB.Close()
+	for rowsB.Next() {
+		var ip string
+		var lastLoginId int
+
+		if err := rows.Scan(&ip, &lastLoginId); err != nil {
+			return ips
+		}
+
+		var count int
+
+		err = db.QueryRow(
+			"SELECT COUNT(1) AS cnt FROM login_log WHERE ip = ? AND ? < id",
+			ip, lastLoginId,
+		).Scan(&count)
+
+		if err != nil {
+			return ips
+		}
+
+		if IPBanThreshold <= count {
+			ips = append(ips, ip)
+		}
+	}
+	if err := rowsB.Err(); err != nil {
+		return ips
+	}
+
+	return ips
+}
+
+func lockedUsers() []string {
+	userIds := []string{}
+
+	rows, err := db.Query(
+		"SELECT user_id, login FROM "+
+			"(SELECT user_id, login, MAX(succeeded) as max_succeeded, COUNT(1) as cnt FROM login_log GROUP BY user_id) "+
+			"AS t0 WHERE t0.user_id IS NOT NULL AND t0.max_succeeded = 0 AND t0.cnt >= ?",
+		UserLockThreshold,
+	)
+
+	if err != nil {
+		return userIds
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var userId int
+		var login string
+
+		if err := rows.Scan(&userId, &login); err != nil {
+			return userIds
+		}
+		userIds = append(userIds, login)
+	}
+	if err := rows.Err(); err != nil {
+		return userIds
+	}
+
+	rowsB, err := db.Query(
+		"SELECT user_id, login, MAX(id) AS last_login_id FROM login_log WHERE user_id IS NOT NULL AND succeeded = 1 GROUP BY user_id",
+	)
+
+	if err != nil {
+		return userIds
+	}
+
+	defer rowsB.Close()
+	for rowsB.Next() {
+		var userId int
+		var login string
+		var lastLoginId int
+
+		if err := rowsB.Scan(&userId, &login, &lastLoginId); err != nil {
+			return userIds
+		}
+
+		var count int
+
+		err = db.QueryRow(
+			"SELECT COUNT(1) AS cnt FROM login_log WHERE user_id = ? AND ? < id",
+			userId, lastLoginId,
+		).Scan(&count)
+
+		if err != nil {
+			return userIds
+		}
+
+		if UserLockThreshold <= count {
+			userIds = append(userIds, login)
+		}
+	}
+	if err := rowsB.Err(); err != nil {
+		return userIds
+	}
+
+	return userIds
 }
